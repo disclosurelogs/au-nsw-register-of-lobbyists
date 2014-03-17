8d906eb9363d764cb156167897b2dd258a7f1b06
Alex Sadleir <maxious@lambdacomplex.org>
2014-03-17 19:24:59 +1100

    upgrade pdf library, split nested records into seperate tables

--- scraper.rb
+++ scraper.rb
@@ -1,47 +1,8 @@
+require 'rubygems'
 require 'scraperwiki'
 require 'pdf-reader'   
 require 'open-uri'
-require 'yaml'
-class Array
-  def to_yaml_style
-    :inline
-  end
-end
 
-##########  This section contains the callback code that processes the PDF file contents  ######
-class PageTextReceiver
-  attr_accessor :content, :page_counter
-  def initialize
-    @content = []
-    @page_counter = 0
-  end
-  # Called when page parsing starts
-  def begin_page(arg = nil)
-    @page_counter += 1
-    @content << ""
-  end
-  # record text that is drawn on the page
-  def show_text(string, *params)
-    if string != ""
-      @content.last << string
-    end
-  end
-
-  def end_text_object(*params)
-    @content << ""
-  end
-
-  # there's a few text callbacks, so make sure we process them all
-  alias :super_show_text :show_text
-  alias :move_to_next_line_and_show_text :show_text
-  alias :set_spacing_next_line_show_text :show_text
-  # this final text callback takes slightly different arguments
-  def show_text_with_positioning(*params)
-    params = params.first
-    params.each { |str| show_text(str) if str.kind_of?(String)}
-  end
-end
-################  End of TextReceiver #############################
 
 
 html = ScraperWiki.scrape("http://www.dpc.nsw.gov.au/programs_and_services/lobbyist_register/who_is_on_register")
@@ -60,40 +21,41 @@ if last_url == "" then resumeFromHere = true end
 
 urls.each do |url|
 
-  if url == last_url or last_url == "" then resumeFromHere = true end
+  if url.to_s == last_url or last_url == "" then resumeFromHere = true end
   if resumeFromHere
     print "Fetching #{url}"
-    ScraperWiki.save_var("last_url", url)
+    ScraperWiki.save_var("last_url", url.to_s)
   
     lines = []
-    lobbyist = {"employees" => [], "clients" => [], "owners" => []}
+    employees = []
+    clients = []
+    owners = []
+    lobbyist_firm = {}
     #######  Instantiate the receiver and the reader
-    receiver = PageTextReceiver.new
-    pdf_reader = PDF::Reader.new 
-    begin
-    pdf = open(url)
-    pdf_reader.parse(pdf, receiver)
     lineno = 0
-    receiver.content.each do |line|    
-       line = line.strip
-       lineno = lineno + 1
-       case line
+    io     = open(url)
+    reader = PDF::Reader.new(io)
+    reader.pages.each do |page|
+     page.text.each_line do |line|
+      line = line.strip()
+      lineno = lineno + 1
+      case line
        when "", "View Lobbyist Details", "Lobbyist Details"
        when /(.*)Details last updated\:(.*)/
            #special case
            lineparts = line.split("Details last updated\:")
            if lineparts.length == 2
-               lobbyist["last_updated"] = lineparts[1]
+               lobbyist_firm["last_updated"] = lineparts[1]
            end
-       when /:/, "Client Details", "Owner Details", "Details of all persons or employees who conduct lobbying activities", "Details last updated:"
+       when /:( |)/, "Client Details", "Owner Details", "Details of all persons or employees who conduct lobbying activities", "Details last updated:"
          #puts "Loading header: #{line}"
          lines << line
        else
          #puts "Loading line: #{line}"
          lines[-1] += " " + line
-       end
+      end
      end
-   
+   end
      in_employees = false
      in_clients = false
      in_owners = false
@@ -106,19 +68,19 @@ urls.each do |url|
        #puts "Processing line: #{line}"
        case line
        when /^Business Entity Name: (.*)/
-         lobbyist["business_name"] = $~[1].strip
-         puts "Processing records for #{lobbyist['business_name']} #{url}"
+         lobbyist_firm["business_name"] = $~[1].strip
+         puts "Processing records for #{lobbyist_firm['business_name']} #{url}"
        when /^ABN: (.*)/
-         lobbyist["abn"] = $~[1].to_s.strip.delete(' ').delete('.').to_i
+         lobbyist_firm["abn"] = $~[1].to_s.strip.delete(' ').delete('.').to_i
        when /^ACN: (.*)/
-         lobbyist["abn"] = $~[1].strip # not strictly true but unique identifier
+         lobbyist_firm["abn"] = $~[1].strip # not strictly true but unique identifier
        when /^Trading Name: (.*)/
          if $~[1].strip != nil then
-           lobbyist["trading_name"] = $~[1].strip
+           lobbyist_firm["trading_name"] = $~[1].strip
          end
        when "Trading Name:"
-         puts "Empty trading name in #{lobbyist['business_name']} #{lobbyist['abn']}, Line =  #{line}"
-       when "Details of all persons or employees who conduct lobbying activities"
+         puts "Empty trading name in #{lobbyist_firm['business_name']} #{lobbyist_firm['abn']}, Line =  #{line}"
+       when "Details of all persons or employees who conduct lobbying activities", 'Details of all persons or employees who conduct lobbying activities Na'
          in_employees = true
          in_clients = false
          in_owners = false
@@ -131,57 +93,53 @@ urls.each do |url|
          in_clients = false
          in_owners = true
        when /^Name: (.*)/
-         name = { "name" => $~[1].strip }
+         name = { "lobbyist_firm_name" => lobbyist_firm["business_name"],"lobbyist_firm_abn" => lobbyist_firm["abn"], "name" => $~[1].strip }
          if in_employees
-           lobbyist["employees"] << name
+           employees << name
          elsif in_clients
-           lobbyist["clients"] << name
+           clients << name
          elsif in_owners
-           lobbyist["owners"] << name
+           owners << name
          else
            raise "Name in an unexpected place '#{line}' #{lineno}"
          end
        when /^Position: (.*)/
          if in_employees
-           lobbyist["employees"].last["position"] = $~[1].strip
+           employees.last["position"] = $~[1].strip
          else
            raise "Position in an unexpected place '#{line}' #{lineno}"
          end
-       when /^Details last updated: (.*)/
+       when /^(Details last up|)dated: (.*)/
          lobbyist["last_updated"] = $~[1].strip      
        when /^including: (.*)/ # special case for some lobbying consortium
-         lobbyist["clients"] << "The Australian Institute of Architects"
-         lobbyist["clients"] << "Consult Australia"
-         lobbyist["clients"] << "CPA Australia"
-         lobbyist["clients"] << "Engineers Australia"
-         lobbyist["clients"] << "The Institute of Chartered Accountants in Australia"
-         lobbyist["clients"] << "The National Institute of Accountants"
-         lobbyist["clients"] << "Professions Australia"
-         lobbyist["clients"] << "Deloitte"
-         lobbyist["clients"] << "Ernst & Young"
-         lobbyist["clients"] << "KPMG"
-         lobbyist["clients"] << "PricewaterhouseCoopers"
-       when /Owner Details Adelaide/
-           lobbyist["owners"] << "See #{url} for details"
-           break; #KPMG have pages and pages of owners
-       when /Please see the following page for owner detail/
-           lobbyist["owners"] << "See #{url} for details"
+         lobbyist["clients"] << { "lobbyist_firm_name" => lobbyist_firm["business_name"],"lobbyist_firm_abn" => lobbyist_firm["abn"], "name" =>"The Australian Institute of Architects"}
+         lobbyist["clients"] << { "lobbyist_firm_name" => lobbyist_firm["business_name"],"lobbyist_firm_abn" => lobbyist_firm["abn"], "name" =>"Consult Australia"}
+         lobbyist["clients"] << { "lobbyist_firm_name" => lobbyist_firm["business_name"],"lobbyist_firm_abn" => lobbyist_firm["abn"], "name" =>"CPA Australia"}
+         lobbyist["clients"] << { "lobbyist_firm_name" => lobbyist_firm["business_name"],"lobbyist_firm_abn" => lobbyist_firm["abn"], "name" =>"Engineers Australia"}
+         lobbyist["clients"] << { "lobbyist_firm_name" => lobbyist_firm["business_name"],"lobbyist_firm_abn" => lobbyist_firm["abn"], "name" =>"The Institute of Chartered Accountants in Australia"}
+         lobbyist["clients"] << { "lobbyist_firm_name" => lobbyist_firm["business_name"],"lobbyist_firm_abn" => lobbyist_firm["abn"], "name" =>"The National Institute of Accountants"}
+         lobbyist["clients"] << { "lobbyist_firm_name" => lobbyist_firm["business_name"],"lobbyist_firm_abn" => lobbyist_firm["abn"], "name" =>"Professions Australia"}
+         lobbyist["clients"] << { "lobbyist_firm_name" => lobbyist_firm["business_name"],"lobbyist_firm_abn" => lobbyist_firm["abn"], "name" =>"Deloitte"}
+         lobbyist["clients"] << { "lobbyist_firm_name" => lobbyist_firm["business_name"],"lobbyist_firm_abn" => lobbyist_firm["abn"], "name" =>"Ernst & Young"}
+         lobbyist["clients"] << { "lobbyist_firm_name" => lobbyist_firm["business_name"],"lobbyist_firm_abn" => lobbyist_firm["abn"], "name" =>"KPMG"}
+         lobbyist["clients"] << { "lobbyist_firm_name" => lobbyist_firm["business_name"],"lobbyist_firm_abn" => lobbyist_firm["abn"], "name" =>"PricewaterhouseCoopers"}
+       when /Owner Details Adelaide/,/Please see the following page for owner detail/
            break; #KPMG have pages and pages of owners
        when /Name:/
            name_next = true
        when /Position:/
            position_next = true
        else
-          if name_next
-            name = { "name" => line.strip }
+          if name_next and line.strip != "Na"
+            name = { "lobbyist_firm_name" => lobbyist_firm["business_name"],"lobbyist_firm_abn" => lobbyist_firm["abn"], "name" => line.strip }
             if in_employees
-              lobbyist["employees"] << name
+               employees << name
                name_next = false
             elsif in_clients
-              lobbyist["clients"] << name
+               clients << name
                name_next = false
             elsif in_owners
-              lobbyist["owners"] << name
+               owners << name
                name_next = false
             else
               raise "Name in an unexpected place '#{line}' #{lineno}"
@@ -189,15 +147,15 @@ urls.each do |url|
           end
           if position_next
             if in_employees
-              lobbyist["employees"].last["position"] = $~[1].strip
+              employees.last["position"] = $~[1].strip
               position_next = false
             else
               raise "Position in an unexpected place '#{line}' #{lineno}"
             end
           end
           if line == "Pegasus: Riding for the Disabled"
-              name = { "name" => line.strip }
-              lobbyist["clients"] << name
+              name = name = { "lobbyist_firm_name" => lobbyist_firm["business_name"],"lobbyist_firm_abn" => lobbyist_firm["abn"], "name" => line.strip }
+              clients << name
               name_next = false
           else 
              raise "Don't know what to do with: '#{line}' #{lineno}"  
@@ -205,14 +163,11 @@ urls.each do |url|
            
        end
      end 
-     lobbyist["employees"] = lobbyist["employees"].to_yaml
-     lobbyist["clients"] = lobbyist["clients"].to_yaml
-     lobbyist["owners"] = lobbyist["owners"].to_yaml
     
-     ScraperWiki.save(unique_keys=["business_name","abn"],data=lobbyist)
-     rescue Timeout::Error => e
-        print "Timeout on #{url}"
-     end
+     ScraperWiki.save(unique_keys=["name","lobbyist_firm_abn"],data=employees, table_name="lobbyists")
+     ScraperWiki.save(unique_keys=["name","lobbyist_firm_abn"],data=clients, table_name="lobbyist_clients")
+     ScraperWiki.save(unique_keys=["name","lobbyist_firm_abn"],data=owners, table_name="lobbyist_firm_owners")
+     ScraperWiki.save(unique_keys=["business_name","abn"],data=lobbyist_firm, table_name="lobbyist_firms")
   else
     print "Skipping #{url}"
   end

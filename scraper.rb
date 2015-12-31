28de44b27d491c0a0f8af53b4df60239743fa32a
Alexander Sadleir <maxious@lambdacomplex.org>
2015-12-31 18:43:29 +1100

    Update to use 'XLS' (actually HTML table) export

--- scraper.rb
+++ scraper.rb
@@ -1,81 +1,82 @@
-require 'rubygems'
 require 'scraperwiki'
-require 'open-uri'
-base_url = "http://www.lobbyists.elections.nsw.gov.au/"
-listing = ScraperWiki.scrape("http://www.lobbyists.elections.nsw.gov.au/whoisontheregister")
+require 'yaml'
+require 'openssl'
+class Array
+  def to_yaml_style
+    :inline
+  end
+end
+require 'net/https'
+require 'uri'
+
+html = "" #open("LobbyExport.html")
+uri = URI.parse("http://www.lobbyists.elections.nsw.gov.au")
+http = Net::HTTP.new(uri.host, uri.port)
+http.use_ssl = true if uri.scheme == "https"  # enable SSL/TLS
+http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+http.start {
+  http.request_get("/LobbyExportXLS") {|res|
+    html = res.body
+  }
+}
 
 # Next we use Nokogiri to extract the values from the HTML source.
 
 require 'nokogiri'
-require 'hpricot'
-page = Hpricot(listing)
+page = Nokogiri::HTML(html)
+headers = page.search('th').map { |a| a.text }
+entityname = ""
 
-urls = page.search('table.list').search('a').map {|a|
-  if a.attributes['href'].include? 'Whois'
-    a.attributes['href']
-  end}.compact
+employees = []
+clients = []
+owners = []
+lobbyist_firm = {}
 
-# resume from the last incomplete url if the scraper was terminated
-resumeFromHere = false
-last_url = ScraperWiki.get_var("last_url", "")
-if last_url == "" then resumeFromHere = true end
+for tr in page.at('tbody').search('tr')
+  #thanks http://ponderer.org/download/xpath/ and http://www.zvon.org/xxl/XPathTutorial/Output/
+  values = headers.zip(tr.search('td').map { |a| a.text })
+  row_data = {}
+  for value in values
+    row_data[value[0]] = value[1]
+  end
+  puts row_data
 
-urls.each do |url|
+  if entityname != row_data['Entity Name'] and entityname != ''
+    #save last
+    ScraperWiki.save(unique_keys=["name", "lobbyist_firm_abn"], data=employees, table_name="lobbyists")
+    ScraperWiki.save(unique_keys=["name", "lobbyist_firm_abn"], data=clients, table_name="lobbyist_clients")
+    ScraperWiki.save(unique_keys=["name", "lobbyist_firm_abn"], data=owners, table_name="lobbyist_firm_owners")
+    ScraperWiki.save(unique_keys=["business_name", "abn"], data=lobbyist_firm, table_name="lobbyist_firms")
 
-  if url.to_s == last_url or last_url == "" then resumeFromHere = true end
-  if resumeFromHere
-    ScraperWiki.save_var("last_url", url.to_s)
-    puts "Downloading #{url}" 
-    begin
-      lobbyhtml = ScraperWiki.scrape("#{base_url}#{url}")
-      lobbypage = Nokogiri::HTML(lobbyhtml)
-  
-      #thanks http://ponderer.org/download/xpath/ and http://www.zvon.org/xxl/XPathTutorial/Output/
-      employees = []
-      clients = []
-      owners = []
-      lobbyist_firm = {}
-  
-      companyABN=lobbypage.xpath('//span[@id="j_id0:j_id8:j_id11:j_id17:ABN"]/a/text()')
-      companyName=lobbypage.xpath('//span[@id="j_id0:j_id8:j_id11:j_id14:businessEntityName"]/text()').first.to_s.gsub(/&amp;/, '&')
-      tradingName=lobbypage.xpath('//span[@id="j_id0:j_id8:j_id11:j_id20:tradingName"]/text()').first.to_s.gsub(/&amp;/, '&')
-      lobbyist_firm["business_name"] = companyName.to_s
-      lobbyist_firm["trading_name"] = tradingName.to_s
-      lobbyist_firm["abn"] =  companyABN.to_s
-      lobbypage.xpath('//span[@id="j_id0:j_id8:j_id11:j_id32:owners"]/text()').each do |owner|
-        ownerNames = owner.content.gsub(/\u00a0/, '').strip.split(',')
-        for ownerName in ownerNames
-          if ownerName.empty? == false and ownerName.class != 'binary'
-              owners << { "lobbyist_firm_name" => lobbyist_firm["business_name"],"lobbyist_firm_abn" => lobbyist_firm["abn"], "name" => ownerName.strip }
+    # reset for next lobbyist
+    employees = []
+    clients = []
+    owners = []
+    lobbyist_firm = {}
+  end
+  entityname = row_data['Entity Name']
 
-          end
-        end
-      end
-      lobbypage.xpath('//tbody[@id="j_id0:j_id43:0:j_id44:j_id46:tb"]/tr').each do |client|
-        clientName = client.content.gsub(/\u00a0/, '').strip
-        if clientName.empty? == false and clientName.class != 'binary'
-            clients << { "lobbyist_firm_name" => lobbyist_firm["business_name"],"lobbyist_firm_abn" => lobbyist_firm["abn"], "name" => clientName }
-        end
-      end
-      lobbypage.xpath('//tbody[@id="j_id0:j_id34:0:j_id35:j_id37:tb"]/tr').each do |employee|
-        employeeName = employee.search('td').first.content.gsub(/\u00a0/, '').gsub("  ", " ").strip
-        employeePosition = employee.search('td').last.content.gsub(/\u00a0/, '').gsub("  ", " ").strip
-
-        if employeeName.empty? == false and employeeName.class != 'binary'
-            employees << { "lobbyist_firm_name" => lobbyist_firm["business_name"],"lobbyist_firm_abn" => lobbyist_firm["abn"], "name" => employeeName, "position" => employeePosition}
-        end
-      end 
-      lobbyist_firm["last_updated"] = lobbypage.xpath('//span[@id="j_id0:j_id8:j_id11:j_id23:detailsLastUpdated"]/text()').to_s
-
-     ScraperWiki.save(unique_keys=["name","lobbyist_firm_abn"],data=employees, table_name="lobbyists")
-     ScraperWiki.save(unique_keys=["name","lobbyist_firm_abn"],data=clients, table_name="lobbyist_clients")
-     ScraperWiki.save(unique_keys=["name","lobbyist_firm_abn"],data=owners, table_name="lobbyist_firm_owners")
-     ScraperWiki.save(unique_keys=["business_name","abn"],data=lobbyist_firm, table_name="lobbyist_firms")
-    rescue Timeout::Error => e
-      print "Timeout on #{url}"
-    end
+  companyABN = row_data['ABN']
+  companyName = row_data['Entity Name']
+  lobbyist_firm["business_name"] = companyName
+  lobbyist_firm["trading_name"] = row_data['Trading Name']
+  lobbyist_firm["abn"] = companyABN
+  lobbyist_firm["last_updated"] = ""
+  lobbyist_firm["status"] = row_data['Status']
+  lobbyist_firm["registration_begins"] = row_data["Registration Begins"]
+  lobbyist_firm["registration_ends"] = row_data["Registration Ends"]
+  if row_data["Contact Type"] == "Client"
+    clients << {"lobbyist_firm_name" => lobbyist_firm["business_name"], "lobbyist_firm_abn" => lobbyist_firm["abn"], "name" => row_data["Contact Name"]}
+  elsif row_data["Contact Type"] == "Employee"
+    employees << {"lobbyist_firm_name" => lobbyist_firm["business_name"], "lobbyist_firm_abn" => lobbyist_firm["abn"], "name" => row_data["Contact Name"]}
+  elsif row_data["Contact Type"] == "Owner"
+    owners << {"lobbyist_firm_name" => lobbyist_firm["business_name"], "lobbyist_firm_abn" => lobbyist_firm["abn"], "name" => row_data["Contact Name"]}
   else
-    print "Skipping #{url}"
+    puts "error unknown contact type: "+data.to_yaml
   end
 end
-ScraperWiki.save_var("last_url", "")
+
+ScraperWiki.save(unique_keys=["name", "lobbyist_firm_abn"], data=employees, table_name="lobbyists")
+ScraperWiki.save(unique_keys=["name", "lobbyist_firm_abn"], data=clients, table_name="lobbyist_clients")
+ScraperWiki.save(unique_keys=["name", "lobbyist_firm_abn"], data=owners, table_name="lobbyist_firm_owners")
+ScraperWiki.save(unique_keys=["business_name", "abn"], data=lobbyist_firm, table_name="lobbyist_firms")
